bc50086aaa2ec2f1a48f32dba5f7ad306036a8ef
Kristina Groeger <7462118+kr1st1n4gr03g3r@users.noreply.github.com>
2025-03-12 17:27:46 -0400

    Send a reload event to the browser when files change.

--- server/server.js
+++ server/server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const chokidar = require("chokidar");
 const cors = require("cors");
-const path = require("path");
 
 const app = express();
 const PORT = 3000;
@@ -12,17 +11,22 @@ app.use(express.static("watched_folder"));
 let clients = [];
 
 app.get("/subscribe", (req, res) => {
+  res.setHeader("Content-Type", "text/event-stream");
+  res.setHeader("Cache-Control", "no-cache");
+  res.setHeader("Connection", "keep-alive");
   clients.push(res);
+
   req.on("close", () => {
     clients = clients.filter((client) => client !== res);
   });
 });
 
 const notifyClients = () => {
-  clients.forEach((client) => client.json({ reload: true }));
+  clients.forEach((client) => client.write("data: reload\n\n"));
   clients = [];
 };
 
+// Watch for CSS/JS changes and notify clients
 chokidar.watch("watched_folder").on("change", notifyClients);
 
 app.listen(PORT, () => {
